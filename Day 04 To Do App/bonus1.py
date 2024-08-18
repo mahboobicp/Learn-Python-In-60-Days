17011acfba743ae0fffc0c6ec3cf839d090c13d5
mahboobicp <mahboobicp@gmail.com>
2024-08-18 15:38:19 +0500

    Day 04

--- Day 04 To Do App/bonus1.py
+++ Day 04 To Do App/bonus1.py
@@ -1,8 +1,11 @@
-password = input("Enter the Password : ")
+# String operation replace('.','-',1) - immutable
+# Tuples imputable e,g myTuple = ('A', 'B')
+# List are mutable e.g mylist = ['A','B']
 
-while password != "pass123":
-    password = input("Enter the Password : ")
-
-print("Password is correct")
-
-
+myBooks = ['1. Physics' , '2. Maths' , '3. Chemesitry']
+for book in myBooks:
+    print(f"In List formate :{book}")
+    print('***'*10)
+    book = book.replace('.','-',1)      # This string method will replace . in book name
+    print(f"In String form : {book}")
+    print('***'*10)
