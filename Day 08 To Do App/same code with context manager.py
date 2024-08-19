dd500d8e1486cccd1e9697daa51651ffe957c2be
mahboobicp <mahboobicp@gmail.com>
2024-08-19 19:19:22 +0500

    Automat to do all feature with context manager

--- Day 08 To Do App/same code with context manager.py
+++ Day 08 To Do App/same code with context manager.py
@@ -19,23 +19,31 @@ while True:
         case 'show':
             with open(path , 'r') as file:
                 todos = file.readlines()
-    
-            """ new_todos = []
-            for item in todos:
-                new_item = item.strip('\n')
-                new_todos.append(new_item) """
-            new_todos =[item.strip('\n') for item in todos] # Do the same with List Comprehensioin
+            new_todos =[item for item in todos] # Do the same with List Comprehensioin
             for index , value in enumerate(new_todos):      # The same can be done with direct method using print(row,end='')
             #   value = value.strip('\n')                   # same result can be achived with this
                 row = f"{index + 1}-{value.title()}"
-                print(row)
+                print(row, end='')
         case 'edit':
             number = int(input("Enter number of To Do in list to Edit : "))
-            number -= 1                                 # list indexing start from 0
-            todos[number] = input("Enter the new To Do : ")
+            number -= 1
+            with open(path , 'r') as file:
+                todos = file.readlines()
+            print(todos)    
+            new_todos = input("Enter the new To Do : ")
+            todos[number] =  new_todos+'\n'
+            print(todos)
+            with open(path , 'w') as file:
+                file.writelines(todos)
+            
         case 'complete':                                #  Complete Feature using pop method 
             number = int(input("Enter number of To Do in list to Complete : "))
-            todos.pop(number - 1)
+            index = number - 1
+            with open(path , 'r') as file:
+                todos = file.readlines()
+            todos.pop(index)
+            with open(path , 'w') as file:
+                file.writelines(todos)
         case 'exit':
             break
         case _:
