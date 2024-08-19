4c000c1cba69271de373ef890c9f8e616775f417
mahboob-office <mahboobicp@gmail.com>
2024-08-19 14:08:43 +0500

    Day 6 final commit

---
+++ Day 07 To Do App/ToDoApp with file.py
@@ -0,0 +1,42 @@
+# In this exercise we will work with files to save the user input data
+while True:
+    print("***"*10)
+    uaction = input("Enter add , show , edit , complete or exit : ")
+    user_action = uaction.lower()
+    user_action = user_action.strip()        # Remove The spaces
+    print("***"*10)
+    path = r"D:\Python\Learn-Python-In-60-Days\Day 06 To Do App\todos.txt"
+    match user_action:
+        case 'add':
+            todo = input("Enter To Do : ") + '\n'
+
+            file = open(path , 'r')                     # Will open the file in read mode
+            todos = file.readlines()                    # Read all the line from the file and return as list
+            file.close()                                # After reading the data close the file
+            
+            todos.append(todo)                          # Add the user input to the list
+            
+            file = open(path , 'w')                     # Open the file in write mode for writing the new input
+            file.writelines(todos)                      # Write the lines to the file
+            file.close()                                # Close
+        case 'show':
+            file = open(path , 'r')                     # To read all the data from file and display it
+            todos = file.readlines()
+            file.close()
+           
+            for index , value in enumerate(todos):
+                row = f"{index + 1}-{value.title()}"
+                print(row , end='')
+        case 'edit':
+            number = int(input("Enter number of To Do in list to Edit : "))
+            number -= 1                                 # list indexing start from 0
+            todos[number] = input("Enter the new To Do : ")
+        case 'complete':                                #  Complete Feature using pop method 
+            number = int(input("Enter number of To Do in list to Complete : "))
+            todos.pop(number - 1)
+        case 'exit':
+            break
+        case _:
+            print("Try Again Incorrect option")
+    
+print("Bye___")
