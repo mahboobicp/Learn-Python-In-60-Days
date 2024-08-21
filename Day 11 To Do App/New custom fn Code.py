f798b7980dbeb8283ef4fe4927f5bf7e52fff590
mahboob-office <mahboobicp@gmail.com>
2024-08-21 10:22:59 +0500

    implement function with arguments

--- Day 11 To Do App/New custom fn Code.py
+++ Day 11 To Do App/New custom fn Code.py
@@ -1,8 +1,12 @@
-def get_todo():
-    with open(path , 'r') as file:
+def get_todo(path_local):
+    with open(path_local , 'r') as file:
         todo_local = file.readlines()
     return todo_local
 
+
+def write_todos(path_local,todo_list):
+    with open(path_local , 'w') as file:
+            file.writelines(todo_list)
 
 while True:    
     uaction = input("\nEnter add, show, edit, complete or exit: ")
@@ -13,14 +17,14 @@ while True:
     if 'add' in user_action or 'new' in user_action:   # Boolean Operator or , and , Not in
         todo = user_action[4:]
         
-        todos = get_todo()
+        todos = get_todo(path)
 
         todos.append(todo+'\n')                          
         
-        with open(path , 'w') as file:
-            file.writelines(todos)
+        write_todos(path , todos)
+
     elif 'show' in user_action:
-        todos = get_todo()
+        todos = get_todo(path)
         new_todos =[item for item in todos] 
         for index , value in enumerate(new_todos):                       
             row = f"{index + 1}-{value.title()}"
@@ -28,21 +32,20 @@ while True:
     elif 'edit' in user_action:
         number = int(user_action[5:])
         number -= 1
-        todos = get_todo()
+        todos = get_todo(path)
         remove_todo = todos[number]   
         new_todos = input("Enter the new To Do : ")
         todos[number] =  new_todos+'\n'
         print(f"New To Do : {new_todos} Removed : {remove_todo}")
-        with open(path , 'w') as file:
-            file.writelines(todos)
+        
+        write_todos(path , todos)
         
     elif 'complete' in user_action:                                
         number = number = int(user_action[9:])
         index = number - 1
-        todos = get_todo()
+        todos = get_todo(path)
         todos.pop(index)
-        with open(path , 'w') as file:
-            file.writelines(todos)
+        write_todos(path , todos)
     elif 'exit' in user_action:
         break
     else:
