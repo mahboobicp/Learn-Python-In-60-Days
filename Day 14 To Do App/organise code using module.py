bdb5adfd42d5d64de641901f4d623166296e94a1
mahboob-office <mahboobicp@gmail.com>
2024-08-22 11:19:35 +0500

    Added time module to show date and time on top

--- Day 14 To Do App/organise code using module.py
+++ Day 14 To Do App/organise code using module.py
@@ -1,5 +1,21 @@
-# from function import get_todo, write_todos       import methods
-import function
+def get_todo(path_local=r"D:\Python\Learn-Python-In-60-Days\Files\todos.txt"): 
+    """ This function open a file in read moode and then read the file 
+    using readlines method and return a List which contain all the file contents """
+    with open(path_local , 'r') as file:
+        todo_local = file.readlines()
+    return todo_local
+
+
+
+def write_todos(todo_list, path_local=r"D:\Python\Learn-Python-In-60-Days\Files\todos.txt"):
+    """ This function take two arguments 
+     1. A list which contain all the data which will be written to the file
+     2. File path """
+    with open(path_local , 'w') as file:
+            file.writelines(todo_list)
+
+
+
 while True:    
     uaction = input("\nEnter add, show, edit, complete or exit: ")
     user_action = uaction.lower()
@@ -8,12 +24,12 @@ while True:
     
     if 'add' in user_action or 'new' in user_action:   # Boolean Operator or , and , Not in
         todo = user_action[4:]
-        todos = function.get_todo(path)
+        todos = get_todo(path)
         todos.append(todo+'\n')                          
-        function.write_todos(path_local=path , todo_list=todos)
+        write_todos(path_local=path , todo_list=todos)
     
     elif 'show' in user_action:
-        todos = function.get_todo(path)
+        todos = get_todo(path)
         new_todos =[item for item in todos] 
         for index , value in enumerate(new_todos):                       
             row = f"{index + 1}-{value.title()}"
@@ -22,19 +38,19 @@ while True:
     elif 'edit' in user_action:
         number = int(user_action[5:])
         number -= 1
-        todos = function.get_todo(path)
+        todos = get_todo(path)
         remove_todo = todos[number]   
         new_todos = input("Enter the new To Do : ")
         todos[number] =  new_todos+'\n'
         print(f"New To Do : {new_todos} Removed : {remove_todo}")
-        function.write_todos(path_local=path , todo_list=todos)     
+        write_todos(path_local=path , todo_list=todos)     
     
     elif 'complete' in user_action:                                
         number = number = int(user_action[9:])
         index = number - 1
-        todos = function.get_todo()
+        todos = get_todo()
         todos.pop(index)
-        function.write_todos(path_local=path , todo_list=todos)
+        write_todos(path_local=path , todo_list=todos)
     
     elif 'exit' in user_action:
         break
--- Day 15 To Do App/function.py
+++ Day 15 To Do App/function.py
@@ -1,4 +1,10 @@
-def get_todo(path_local=r"D:\Learn Python in 60 Days\Files\todos.txt"): 
+FILEPATH = r"D:\Python\Learn-Python-In-60-Days\Files\todos.txt"
+import time
+now = time.strftime("%b %d, %Y %H : %M : %S")
+print("***"*20)
+print("It is ",now)
+print("***"*20)
+def get_todo(path_local=FILEPATH): 
     """ This function open a file in read moode and then read the file 
     using readlines method and return a List which contain all the file contents """
     with open(path_local , 'r') as file:
@@ -7,7 +13,7 @@ def get_todo(path_local=r"D:\Learn Python in 60 Days\Files\todos.txt"):
 
 
 
-def write_todos(todo_list, path_local=r"D:\Learn Python in 60 Days\Files\todos.txt"):
+def write_todos(todo_list, path_local=FILEPATH):
     """ This function take two arguments 
      1. A list which contain all the data which will be written to the file
      2. File path """
