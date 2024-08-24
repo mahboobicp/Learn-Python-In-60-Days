f280b6e9b6222ceb54e38cc2deda28425988e13d
mahboobicp <mahboobicp@gmail.com>
2024-08-24 14:05:10 +0500

    Final Touch of To Do App

--- Day 18 To Do Various GUI/gui.py
+++ Day 18 To Do Various GUI/gui.py
@@ -8,11 +8,12 @@ list_box = sg.Listbox(values=function.get_todo(), key='todos',
 edit_button = sg.Button("Edit")
 completebtn = sg.Button("Complete")
 exitbtn = sg.Button("Exit")
+aa = sg.Text('',key='msg')
+sg.theme("black")
 window = sg.Window("Todo App GUI",[[label],[input_box,btn],
                                    [list_box,edit_button,completebtn],
-                                   [exitbtn]
+                                   [exitbtn,aa]
                                    ])
-
 while True:
     event,values = window.read()
     match event:
@@ -23,22 +24,29 @@ while True:
             function.write_todos(todos)
             window['todos'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + '\n'
-            todos = function.get_todo()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            function.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:    
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + '\n'
+                todos = function.get_todo()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                function.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                window['msg'].update(value="First Select an Item")
+
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case "Complete":
-            todo_to_remov = values["todos"][0]
-            todos = function.get_todo()
-            todos.remove(todo_to_remov)
-            function.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value=' ')
+            try:
+                todo_to_remov = values["todos"][0]
+                todos = function.get_todo()
+                todos.remove(todo_to_remov)
+                function.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value=' ')
+            except IndexError:
+                window['msg'].update(value="First Select an Item")
         case "Exit":
             break
         case sg.WIN_CLOSED:
