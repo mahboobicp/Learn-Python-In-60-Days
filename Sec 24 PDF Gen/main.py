37196599b62f8dc46b9fa0e89e08613dedd5e37b
mahboobicp <mahboobicp@gmail.com>
2024-08-24 16:17:35 +0500

    Multipal pages Generatioin

---
+++ Sec 24 PDF Gen/main.py
@@ -0,0 +1,36 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit='mm', format='a4')
+pdf.set_auto_page_break(auto=False, margin=0)
+df = pd.read_csv("topics.csv")
+
+for index , row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B",size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0,h=12,txt=row["Topic"],align="l",ln=1)
+    # Draw lines
+    pdf.line(10,21,200,21)
+    for x in range(31,280,10):
+        pdf.line(10,x,200,x)
+    
+    # pdf footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I",size=8)
+    pdf.set_text_color(100,100,180)
+    pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
+    
+
+    for i in range(row['Pages']-1):
+        pdf.add_page()
+        # pdf footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I",size=8)
+        pdf.set_text_color(100,100,180)
+        pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
+        # Draw Line 
+        for x in range(31,280,10):
+            pdf.line(10,x,200,x)
+pdf.output("output.pdf")
